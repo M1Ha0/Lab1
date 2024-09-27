844a8c2337f2050bb4ab7b5331e6be6fd22a4385
MiHa0_o <MiHa0_o@209-5>
2024-09-27 18:01:41 +0200

    Добавьте файлы проекта.

---
+++ Lab1/Program.cs
@@ -0,0 +1,11 @@
+﻿try
+{
+        Console.WriteLine("Введите трехзначное число");
+int x = int.Parse(Console.ReadLine());
+int c = x % 10;
+Console.WriteLine($"Число N=:{(x - c) / 10 + c * 100}");
+}
+catch
+{
+    Console.WriteLine("Правильно введите данные");
+}
